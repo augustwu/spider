8c3657ad7ad1812d6bbd64d71dfb7abf44d66a17
wu <wuguojun@chinatelecom.cn>
2017-07-19 17:56:42 +0800

    add pipeline

--- spider/pipelines.py
+++ spider/pipelines.py
@@ -7,14 +7,62 @@
 
 import codecs
 import json
+import MySQLdb
+from datetime import datetime
+
+mysql_con  = MySQLdb.connect(host='localhost',user='hiveuser',passwd='123456',db='wp')
+
 
 class SpiderPipeline(object):
 
-    def __init__(self):
-        self.file = codecs.open('data.json','wb',encoding='utf-8')
+    
 
+    def exists_item(self,title):
+        sql = 'select count(0) from wp_posts where post_title=%s' % title 
+        self.cursor.execute(sql)
+        return self.cursor.fetchone()
+
+    
+    def insert_item(self,item):
+        sql = 'insert into wp_posts(post_author,post_date,post_date_gmt,post_content,
+            post_title,post_excerpt,post_name,post_modified,
+            post_modified_gmt,post_type) values 
+            (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)' % (1,datetime.now(),datetime.now(),item.get('content'),item.get('full_name'),item.get('unique_name'),item.get('full_name'),datetime.now(),datetime.now(),'post')
+
+        self.cursor.execute(sql)
+        return self.cursor.fetchone()
+        
+
+    def exists_category(self,category):
+        sql = 'select count(0) from wp_terms wher name=%s' % category
+        self.cursor.execute(sql)
+        return self.cursor.fetchone()
+
+    def insert_category(self,item):
+        sql = 'insert into wp_terms(name,slug) values(%s,%s)' % (item.get('category'),item.get('category'))
+        self.cursor.execute(sql)
+        return self.cursor.fetchone()
+        
+        
+
+    def __init__(self):
+        self.db = mysql_con
+        self.cursor = db.cursor()
 
     def process_item(self, item, spider):
-        line = json.dumps(dict(item)) + '\n'
-        self.file.write(line.decode("unicode_escape"))
-        return item
+        title = item.get('full_name')
+        category = item.get('category')
+        if self.exists_item(title):
+            pass
+        else:
+            self.insert_item(item)
+            
+      if not self.exists_category(category):
+          self.insert_category(item)
+
+      
+
+
+
+
+  
--- spider/settings.py
+++ spider/settings.py
@@ -65,8 +65,8 @@ ROBOTSTXT_OBEY = True
 # Configure item pipelines
 # See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
-    'spider.pipelines.SpiderPipeline': 300,
-    'scrapy.pipelines.files.FilesPipeline': 1,
+    'spider.pipelines.SpiderPipeline': 1,
+#    'scrapy.pipelines.files.FilesPipeline': 1,
     'scrapy.pipelines.images.ImagesPipeline':1,
 }
 
